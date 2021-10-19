5b560523d3ae6f9a08a9c0daafbef61215668c81
Hero Live <herolivechannel@gmail.com>
2021-10-19 17:10:36 +0700

    Server-socket.io-flutter

--- nodejs_socketIO_flutter/server/index.js
+++ nodejs_socketIO_flutter/server/index.js
@@ -1,3 +1,25 @@
+const { Socket } = require('dgram');
+
+const app = require('express')();
+const server = require('http').createServer(app);
+const options = { /* ... */ };
+const io = require('socket.io')(server, options);
+const port = 3484;
+
+io.on('connection', client => {
+    console.log(`New client connected`);
+    client.on('fromClient', data => { 
+        console.log(data); 
+        client.emit('fromServer', `${Number(data)+1}`)
+    })
+    client.on('disconnect', () => console.log(`Client disconnected`))
+});
+
+server.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+/*
 const express = require('express')
 const app = express()
 const port = 3000
@@ -9,3 +31,4 @@ app.get('/',(req,res)=>{
 app.listen(port, ()=>{
     console.log(`Example app listening at http://localhost:${port}`)
 })
+*/
